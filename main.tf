62f0a709ef0a20a05b86a24982c36a928521eccf
MnJ <ioannispaliakis@gmail.com>
2024-01-21 01:22:02 +0200

    cd

--- main.tf
+++ main.tf
@@ -42,7 +42,7 @@ resource "azurerm_app_service_plan" "more_service_plan" {
 
 }
 
-resource "azurerm_app_service" "more_app_service" {
+resource "azurerm_app_service" "more_app_service_front" {
   name                = "app-more-webapp-fronent-we"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
